ee7bf2c448f2acbf82c110f925b621ec836297ec
mnolan34 <83682308+mnolan34@users.noreply.github.com>
2021-11-28 14:19:06 -0500

    Unit Tests Pass

--- unitTesting.py
+++ unitTesting.py
@@ -2,33 +2,44 @@ from zenDeskCode import ticketManager
 
 class unitTests:
 
-	#Test printTicket with int negative
-	def unitTest1():
-		test1 = ticketManager()
-		test1.printTicket(-1)
-		print("Above should print Ticket does not exist")
+        #Test printTicket with int negative
+        def unitTest1():
+                test1 = ticketManager()
+                test1.connect()
+                test1.printTicket(-1)
+                print("Above should print Ticket does not exist")
 
-	#Test printTicket with int too high
-	def unitTest2():
-		test2 = ticketManager()
-		test2.printTicket(1000)
-		print("Above should print Ticket does not exist")
+        #Test printTicket with int too high
+        def unitTest2():
+                test2 = ticketManager()
+                test2.connect()
+                test2.printTicket(1000)
+                print("Above should print Ticket does not exist")
 
-	#Test printTicket with string
-	def unitTest3():
-		test3 = ticketManager()
-		test3.printTicket('blue')
-		print("Above should print Please enter an integer value")
+        #Test printTicket with string
+        def unitTest3():
+                test3 = ticketManager()
+                test3.connect()
+                test3.printTicket('blue')
+                print("Above should print Please enter an integer value")
 
-	#Test nextPage end page
-	def unitTest4():
-		test4 = ticketManager()
-		test4.pageNum = 3
-		test4.nextPage()
-		print("Above should say Last Page Reached")
+        #Test nextPage end page
+        def unitTest4():
+                test4 = ticketManager()
+                test4.connect()
+                test4.pageNum = 3
+                test4.nextPage()
+                print("Above should say Last Page Reached")
 
-	#Test prevPage beginning page
-	def unitTest5():
-		test5 = ticketManager()
-		test5.prevPage()
-		print("ABove should say first page reached")
+        #Test prevPage beginning page
+        def unitTest5():
+                test5 = ticketManager()
+                test5.connect()
+                test5.prevPage()
+                print("ABove should say first page reached")
+
+        unitTest1()
+        unitTest2()
+        unitTest3()
+        unitTest4()
+        unitTest5()
