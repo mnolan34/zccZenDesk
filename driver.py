2aaf8fcac882223b254ad27382e8de1d34b96bc0
mnolan34 <83682308+mnolan34@users.noreply.github.com>
2021-11-27 21:21:32 -0500

    Completed Driver

--- driver.py
+++ driver.py
@@ -1,4 +1,4 @@
-import zenDeskCode
+from ZCCZen import zenDeskCode
 
 class driver:
 
@@ -6,7 +6,31 @@ class driver:
         task = ticketManager()
         data = task.connect
         data = factorData()
-
+        print('Welcome to the ticket viewer')
+        print("Type'menu' to view options or 'quit' to exit")
+        command = input()
+        while command != 'q':
+            #ListCommand
+            if(command == 'l'):
+                task.listTickets()
+            #Menu Command
+            elif(command == 'menu'):
+                task.menuList()
+            #Quit Command
+            elif(command == 'quit'):
+                command = 'q'
+            #nextPage Command
+            elif(command == 'n'):
+                task.nextPage()
+            #prevPage Command
+            elif(command == 'p'):
+                task.prevPage()
+            #specificTicket Command
+            elif(command == 't'):
+                continue
+            #If command unrecognized, ask again
+            else:
+                print("Command unrecognized: please try again")
 
     if __name__ == "__main__":
         main()
